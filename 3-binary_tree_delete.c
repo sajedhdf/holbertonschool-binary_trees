e47e2d2d17c9ade92b9199609287b86880c77249
ronan207 <12341@holbertonstudents.com>
2025-12-12 13:19:17 +0100

    i have corriged the betty style

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,16 +1,18 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_delete - deletes an entire binary tree
- * @tree: pointer to the root node
+ * binary_tree_delete - Deletes an entire binary tree
+ * @tree: Pointer to the root node of the tree to delete
+ *
+ * Description: This function frees all nodes of a binary tree.
  */
 void binary_tree_delete(binary_tree_t *tree)
+
 {
-    if (tree == NULL)
-        return;
-
-    binary_tree_delete(tree->left);
-    binary_tree_delete(tree->right);
-
-    free(tree);
+	if (!tree)
+		return;
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
 }
+
